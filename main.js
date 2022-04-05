451d51ec4b75d2354e2a43ad3efa59083045900d
daniel nagar <danielnagar80@gmail.com>
2022-04-05 19:44:44 +0300

    add class, shuffle

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const cards = ["A", "B", "C", "D", "E", "A", "B", "C", "D", "E"];
 
+function AddinDivElement(v, i, arr) {
 
-function AddinDivElement(v,i,arr){
-const element = document.createElement("div");
-element.innerHTML = v
-const board = document.getElementById("board")
-board.appendChild(element)
+    const element = document.createElement("div");
+    element.innerHTML = v
+    element.id = i
+    element.className = "card"
+    const board = document.getElementById("board")
+    board.appendChild(element)
 }
-
+shuffle(cards)
 cards.filter(AddinDivElement)
+
+
+
+// htmlפונקצייה שמערבבת את המערך ואז שם ב 
+function shuffle(cards) {
+    cards = cards.sort(() => Math.random() - 0.5)
+}
 
